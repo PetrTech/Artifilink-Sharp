1597f41f69dafd78540c089c1959ad002a807fc1
PetrTech <55279432+PetrTech@users.noreply.github.com>
2022-07-27 20:14:14 +0200

    Add project files.

---
+++ ArtifilinkSharp/Action.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtifilinkSharp
+{
+    class Action
+    {
+        public enum setActionType {
+            keyPress,
+            moveMouse,
+            lmbMouse,
+            rmbMouse,
+            mmbMouse,
+            integer,
+            text,
+            floatingPoint
+        };
+
+        public setActionType actionType;
+
+        // key press
+        public Action()
+        {
+
+        }
+    }
+}
---
+++ ArtifilinkSharp/Neural.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ArtifilinkSharp
+{
+    public class Neural
+    {
+        string[] inputted;
+        
+        public Neural(string[] input, int hiddenLayerCount, bool saveEachGeneration, string location)
+        {
+            Debug.WriteLine("[Artifilink] <NEURAL NETWORK> - Initializing");
+
+            if(input.Length < 2)
+            {
+                Debug.WriteLine("[Artifilink] <NEURAL NETWORK> - Initialization succeeded, but there were some problems: ");
+                Console.WriteLine("[Artifilink] <NEURAL NETWORK> - Initialization succeeded, but there were some problems: ");
+
+                Debug.WriteLine("Not enough input data. Received number of data: " + input.Length);
+                Console.WriteLine("Not enough input data. Received number of data: " + input.Length);
+            }
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
